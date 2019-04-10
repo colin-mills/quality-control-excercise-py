425b67ca85c3ed6adf906b21ba2856a10be324d7
Colin Mills <cgm71@georgetown.edu>
2019-04-10 18:45:03 -0400

    second test working

--- app/inventory_app.py
+++ app/inventory_app.py
@@ -13,12 +13,24 @@ def read_from_csv(my_csv_filepath):
         reader = csv.DictReader(csv_file)
         for ordered_dict in reader:
             products.append(dict(ordered_dict))
-
     return products
+
+def write_to_file(my_products, my_filepath):
+        with open(my_filepath, "w") as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+            writer.writeheader()
+
+        for product in my_products:
+            writer.writerow(product)
+
+def autogen_id(products):
+    ID = len(products) + 1
+    return ID
 #
 # READ
 #
 
+#Only runs if script is run from command line
 if __name__ == "__main__":
 
     products = read_from_csv(csv_filepath)
@@ -52,7 +64,7 @@ if __name__ == "__main__":
         "price": 99.99
     } # further exploration / todo: ask for user to input
 
-    next_id = len(products) + 1
+    next_id = autogen_id(products)
 
     new_product["id"] = next_id
 
@@ -64,9 +76,10 @@ if __name__ == "__main__":
     # WRITE
     #
 
-    with open(csv_filepath, "w") as csv_file:
-        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
-        writer.writeheader()
-
-        for product in products:
-            writer.writerow(product)
+    write_to_file(products, csv_filepath)
+    #with open(csv_filepath, "w") as csv_file:
+    #    writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+    #    writer.writeheader()
+#
+    #    for product in products:
+    #        writer.writerow(product)
--- test/inventory_test.py
+++ test/inventory_test.py
@@ -1,7 +1,9 @@
 import csv
 import os
 
-from app.inventory_app import read_from_csv
+from app.inventory_app import read_from_csv 
+from app.inventory_app import write_to_file
+#from app.inventory_app import autogen_id
 
 #from app.inventory_app import read_from_csv, write_to_csv, autogen_id
 
@@ -15,22 +17,30 @@ def test_read_from_csv():
     assert default_products[0]["name"] == "Chocolate Sandwich Cookies" 
 
 
-## TODO: test logic for writing a list of product dictionaries to a CSV file
-#def test_write_to_csv():
-#    temp_filepath = os.path.join(os.path.dirname(__file__), "..", "test", "mock_data", "temp_products.csv")
-#
-#    if os.path.isfile(temp_filepath):
-#        os.remove(temp_filepath)
-#
-#    assert os.path.isfile(temp_filepath) == False
-#
-#    products = []
-#
-#    #write_to_file(products, temp_filepath)
-#
-#    assert os.path.isfile(temp_filepath) == True
-#
-#
-## TODO: test logic for auto-incrementing the identifier of a product about to be created
-#def test_autogen_id():
-#    assert False
+# TODO: test logic for writing a list of product dictionaries to a CSV file
+def test_write_to_csv():
+    temp_filepath = os.path.join(os.path.dirname(__file__), "..", "test", "mock_data", "temp_products.csv")
+
+    if os.path.isfile(temp_filepath):
+        os.remove(temp_filepath)
+
+    assert os.path.isfile(temp_filepath) == False
+
+    products = []
+
+    write_to_file(products, temp_filepath)
+
+    assert os.path.isfile(temp_filepath) == True
+
+
+# TODO: test logic for auto-incrementing the identifier of a product about to be created
+def test_autogen_id():
+    products = read_from_csv
+    next_ID = autogen_id(products)
+    assert next_ID == 1
+
+    #default_products_filepath = os.path.join(os.path.dirname(__file__), "..", "test", "mock_data", "default_products.csv")
+    #default_products = read_from_csv(default_products_filepath)
+    #breakpoint()
+    #new_ID = autogen_id(default_products)
+    assert True
